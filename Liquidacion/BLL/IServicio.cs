2f8b956e41835e4d1927d55bb49450163d5e99ce
Victor Manuel Rios Pertuz <70718689+VictorManuel19@users.noreply.github.com>
2020-10-08 19:20:59 -0500

    Add files via upload

---
+++ Liquidacion/BLL/IServicio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public interface IServicio
+    {
+        bool Guardar(object datos);
+        bool Update(object datos);
+        bool Delete(object datos);
+        bool Read(object datos);
+
+        object ReadAll();
+
+    }
+}
